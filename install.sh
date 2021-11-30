cdacb98d130a8919f47f0dd39bfd4bd83f9bd25e
Nextgen-Networks <reg-github.com@nextgen-networks.de>
2021-11-30 22:14:10 +0100

    Update install.sh
    Change source and naming scheme

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SourcePath=https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master
+SourcePath=https://raw.githubusercontent.com/nextgen-networks/retroflag-picase/main
 
 #Check if root--------------------------------------
 if [[ $EUID -ne 0 ]]; then
